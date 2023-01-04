14f1e564bed8efcc63aa420f7393a6f086d829e0
Aiden Cullo <aidencullo@gmail.com>
2023-01-04 16:59:05 -0500

    got rid of package files

--- .emacs.d/elpa/grep-a-lot-20210618.1420/grep-a-lot-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from grep-a-lot.el  -*- no-byte-compile: t -*-
-(define-package "grep-a-lot" "20210618.1420" "manages multiple search results buffers for grep.el" 'nil :commit "223819dbea049bdeb5f97f9849fce139a5f16a75" :authors '(("Avi Rozen" . "avi.rozen@gmail.com")) :maintainer '("Avi Rozen" . "avi.rozen@gmail.com") :keywords '("tools" "convenience" "search") :url "https://github.com/ZungBang/emacs-grep-a-lot")
