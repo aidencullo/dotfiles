ade255b6ceac1f07f9e976670b2209922a8c2c77
Aiden Cullo <aidencullo@gmail.com>
2023-07-12 18:16:23 -0300

    reinstalling emacs -- can't get vue mode working

--- emacs/init.el
+++ emacs/init.el
@@ -34,7 +34,7 @@
 ;; turn on auto-fill mode for all text buffers
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 ;; turn on auto-fill mode in all major modes
-(setq-default auto-fill-function 'do-auto-fill)
+;; (setq-default auto-fill-function 'do-auto-fill)
 
 ;;
 ;; Settings
@@ -94,7 +94,7 @@
  '(grep-find-ignored-files
    '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "*-lock.*"))
  '(package-selected-packages
-   '(vue-mode yasnippet yaml-mode use-package typescript-mode swift-mode spinner paredit monokai-theme modus-themes markdown-mode magit lv ht grep-a-lot f expand-region company auto-complete)))
+   '(vue-mode lsp-mode yasnippet yaml-mode use-package typescript-mode swift-mode spinner paredit monokai-theme modus-themes markdown-mode magit lv ht grep-a-lot f expand-region company auto-complete)))
 
 ;;
 ;; packages
